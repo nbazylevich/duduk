bd8a26cf259596ce03c584d202bda210bc6af3e9
Nbazylevich <nbazylevich@gmail.com>
2017-12-28 22:57:16 +0300

    mySlider start

--- wp-content/themes/themeDuduk/index.php
+++ wp-content/themes/themeDuduk/index.php
@@ -2,10 +2,20 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>DUDUKIST</title>
     <?php wp_head() ?>
 </head>
 <body>
 <div>
     Профессиональный дудук от лучших мастеров Армении с пожизненной гарантией
+
+    <div class="mySlider posRelative">
+        <div>
+            <img src="<?php bloginfo('template_url')?>/picture/1.jpg" class="sliderImg active" alt="">
+            <img src="<?php bloginfo('template_url')?>/picture/2.jpg" class="sliderImg" alt="">
+            <img src="<?php bloginfo('template_url')?>/picture/2.jpg" class="sliderImg" alt="">
+        </div>
+        <div class="sliderArrowRight text-center cursor">></div>
+        <div class="sliderArrowLeft text-center cursor"><</div>
+    </div>
 </div>
