7fd23451b137cd7307d2e6e0fe6f04a697fdb274
Nbazylevich <nbazylevich@gmail.com>
2017-12-28 19:53:49 +0300

    index.php start

--- wp-content/themes/themeDuduk/index.php
+++ wp-content/themes/themeDuduk/index.php
@@ -1,1 +1,10 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+<div>
+    Профессиональный дудук от лучших мастеров Армении с пожизненной гарантией
+</div>
